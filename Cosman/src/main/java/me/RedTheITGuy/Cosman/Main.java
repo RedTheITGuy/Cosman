ee7a6525d64c64c648d94174da128da43c2b9ac2
red <red@localhost.localdomain>
2020-09-03 14:12:23 +0100

    Added plugin components

---
+++ Cosman/src/main/java/me/RedTheITGuy/Cosman/Main.java
@@ -0,0 +1,10 @@
+package me.RedTheITGuy.Cosman;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	// Fired when the plugin is first enabled
+	@Override
+	public void onEnable() {
+		// Code here
+	}
+}
